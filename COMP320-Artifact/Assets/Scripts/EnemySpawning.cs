5450efe9c341877aa30983ccdf2cc89e7b76e53d
Daniel Neale <dn230726@falmouth.ac.uk>
2022-03-13 21:54:15 +0000

    Enemies respawn at checkpoints

--- COMP320-Artifact/Assets/Scripts/EnemySpawning.cs
+++ COMP320-Artifact/Assets/Scripts/EnemySpawning.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class EnemySpawning : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject[] enemies;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].SetActive(true);
+        }
+    }
 }
