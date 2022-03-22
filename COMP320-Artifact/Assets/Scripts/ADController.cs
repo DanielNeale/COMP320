9d0a14a4c87c88e0e88a07dfedad82fe991b9773
Daniel Neale <dn230726@falmouth.ac.uk>
2022-03-22 01:16:35 +0000

    Time in view of enemies is now calculated

--- COMP320-Artifact/Assets/Scripts/ADController.cs
+++ COMP320-Artifact/Assets/Scripts/ADController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class ADController : MonoBehaviour
 {
     [SerializeField]
+    private Transform player;
+    [SerializeField]
     private Transform[] enemyParents;
     private List<Transform> enemies = new List<Transform>();
+
+    private Queue<float> timeInSight = new Queue<float>();
+    private List<bool> inSightThisSec = new List<bool>();
 
 
     private void Start()
@@ -16,6 +21,44 @@ public class ADController : MonoBehaviour
             for (int j = 0; j < enemyParents[i].childCount; j++)
             {
                 enemies.Add(enemyParents[i].GetChild(j));
+            }
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        bool inSight = false;
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (Physics.Linecast(transform.position, player.position, out RaycastHit hit) && hit.transform == player)
+            {
+                inSight = true;
+            }
+        }
+
+        inSightThisSec.Add(inSight);
+        
+        if (inSightThisSec.Count >= 50)
+        {
+            int secondAverage = 0;
+
+            for (int i = 0; i < inSightThisSec.Count; i++)
+            {
+                if (inSightThisSec[i])
+                {
+                    secondAverage++;
+                }
+
+                secondAverage /= inSightThisSec.Count;
+
+                timeInSight.Enqueue(secondAverage);
+
+                if (timeInSight.Count > 30)
+                {
+                    timeInSight.Dequeue();
+                }
             }
         }
     }
